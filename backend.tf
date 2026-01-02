6b4fa9e8e2066a81108bccd87d97d84dc0541c3f
Pankaj Verma <137068547+codiebyheaart@users.noreply.github.com>
2026-01-02 18:31:45 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstateXXXXXXXXX"  # Replace with your $STATE_STORAGE value
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
