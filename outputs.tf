c65dd2e91ee4857efda8c426651093a45c45880f
Pankaj Verma <137068547+codiebyheaart@users.noreply.github.com>
2026-01-02 18:33:46 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,29 @@
+output "resource_group_name" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "virtual_network_name" {
+  description = "Name of the virtual network"
+  value       = azurerm_virtual_network.main.name
+}
+
+output "storage_account_name" {
+  description = "Name of the storage account"
+  value       = azurerm_storage_account.main.name
+}
+
+output "vm_public_ip" {
+  description = "Public IP address of the VM"
+  value       = azurerm_public_ip.main.ip_address
+}
+
+output "vm_name" {
+  description = "Name of the virtual machine"
+  value       = azurerm_linux_virtual_machine.main.name
+}
+
+output "admin_username" {
+  description = "Admin username for SSH"
+  value       = azurerm_linux_virtual_machine.main.admin_username
+}
